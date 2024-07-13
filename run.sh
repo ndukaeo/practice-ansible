e8bb3920136899c31d1f8f061b4d74a6583457cf
nduka ojeh <nojeh@baneollc.com>
2024-07-13 17:13:51 -0400

    starting front end setup

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-git pull ; ansible-playbook -i $1.$2.banecio-devops.online,  -e ansible_user=ec2-user -e ansible_password= -e role_name=$1 -e env=$2 main.yml
+git pull ; ansible-playbook -i $1.$2.banecio-devops.online,  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 -e env=$2 main.yml
 
